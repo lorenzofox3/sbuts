829c95cefa18b37cd571aea18a0c2617e61bcad0
Laurent RENARD <laurent34azerty@gmail.com>
2020-05-18 09:21:19 +0200

    remove vc

--- node_modules/zora-node-reporter/dist/declarations/tap/src/util.d.ts
+++
@@ -1,5 +0,0 @@
-import { AssertionResult, Message, Test } from 'zora';
-export declare const map: (fn: Function) => (stream: AsyncIterable<Message<any>>) => AsyncGenerator<any, void, unknown>;
-export declare const flatten: (stream: AsyncIterable<Message<any>>) => AsyncGenerator<any, void, unknown>;
-export declare const isAssertionResult: (result: AssertionResult | Test) => result is AssertionResult;
-export declare const stringifySymbol: (key: string, value: Symbol) => string | Symbol;
